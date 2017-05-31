b78b5a2d07a23c33b389ca53a2327d5c47e00d2d
Robin Singh <robinsingh2121@gmail.com>
2017-05-31 22:02:44 +0530

    added ids to hospitals

--- Doc_final.py
+++ Doc_final.py
@@ -4,13 +4,13 @@ import bs4
 import re
 import numpy as np
 
-links=pd.read_csv('Data_set.csv').LINKS     #use the address of the csv file if not present in same directory.
-hosp_name=pd.read_csv('Data_set.csv')['HOSPITAL NAME']
+links=pd.read_csv('./Data(net)/hosp_sample.csv').LINKS     #use the address of the csv file if not present in same directory.
+hosp_name=pd.read_csv('./Data(net)/hosp_sample.csv')['HOSPITAL NAME']
 df1, df2, df3, df4, df5 = pd.DataFrame([]), pd.DataFrame([]), pd.DataFrame([]), pd.DataFrame([]), pd.DataFrame([])
 a=0             #counter for range of the csv file
 for link,hospital in zip(links,hosp_name):
     for i in range(1,100):
-        link_f=link +'?page='+str(i)+'&ajax=true'
+        link_f = link +'?page='+str(i)+'&ajax=true'
         print(link_f)
         hospi_page = urllib.request.urlopen(link_f).read()
         hospi_html = bs4.BeautifulSoup(hospi_page, 'lxml')
@@ -40,11 +40,9 @@ for link,hospital in zip(links,hosp_name):
         df3=df3.append(df3_temp)
 
         #doctor Specialties
-        dr_spec_raw = hospi_html.find_all('p', attrs = {'class':'doc-specialties'})
+        dr_spec_raw = hospi_html.find_all('a', attrs = {'class':'link grey'})
         dr_spec = [tag.text for tag in dr_spec_raw]
-        dr_spec_final = [a.replace('\n','') for a in dr_spec]
-        dr_spec_final2 = [a.replace(' ','') for a in dr_spec_final]
-        df4_temp = pd.DataFrame(dr_spec_final2)
+        df4_temp = pd.DataFrame(dr_spec)
         df4=df4.append(df4_temp)
 
         #Hospital Name
